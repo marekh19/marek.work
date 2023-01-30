390b1422151dbc09b6bd388c90be93d39bb967b4
Marek Honzal <marek.honzal@gmail.com>
2023-01-30 11:38:25 +0100

    refactor: minor color updates

--- src/features/home/Page/index.tsx
+++ src/features/home/Page/index.tsx
@@ -12,7 +12,7 @@ import { Work } from '../Sections/Work'
 export const Home: FC = () => {
   return (
     <DefaultLayout>
-      <cite className="mx-auto block w-5/6 rounded bg-gray-800 bg-opacity-10 py-3 text-center not-italic shadow-md backdrop-blur-lg dark:bg-gray-50 dark:bg-opacity-10 sm:w-3/4">
+      <cite className="mx-auto block w-5/6 rounded bg-gray-900 bg-opacity-10 py-3 text-center not-italic shadow-md backdrop-blur-lg dark:bg-gray-200 dark:bg-opacity-10 sm:w-3/4">
         Hi, I&apos;m a web developer based in the&nbsp;Czech&nbsp;Republic!
       </cite>
       <Introduction />
